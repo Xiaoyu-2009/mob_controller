ff97f0fcd0e92c1739fa7b183c288776f8bd214b
小宇不是鱼 <3551371733@qq.com>
2026-01-06 16:09:13 +0800

    Add files via upload

--- src/main/java/net/xiaoyu/mob_controller/mixin/HoglinMixin.java
+++ src/main/java/net/xiaoyu/mob_controller/mixin/HoglinMixin.java
@@ -1,41 +1,29 @@
 package net.xiaoyu.mob_controller.mixin;
 
 import net.xiaoyu.mob_controller.util.MobControlledData;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.Brain;
-import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.monster.hoglin.Hoglin;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.*;
 
 @Mixin(Hoglin.class)
 public class HoglinMixin {
 
-    @Inject(method = "customServerAiStep", at = @At("HEAD"))
-    private void onCustomServerAiStep(CallbackInfo ci) {
+    // 被控制的疣猪兽不攻击主人
+    @Inject(method = "doHurtTarget", at = @At("HEAD"), cancellable = true)
+    private void onDoHurtTarget(Entity target, CallbackInfoReturnable<Boolean> cir) {
         Hoglin hoglin = (Hoglin) (Object) this;
         
         if (MobControlledData.isControlledMob(hoglin)) {
-            Brain<Hoglin> brain = hoglin.getBrain();
-            
-            if (brain.hasMemoryValue(MemoryModuleType.ATTACK_TARGET)) {
-                Optional<LivingEntity> attackTarget = brain.getMemory(MemoryModuleType.ATTACK_TARGET);
-                
-                if (attackTarget.isPresent()) {
-                    LivingEntity target = attackTarget.get();
-
-                    if (target instanceof Player) {
-                        UUID controllerUUID = MobControlledData.getControllerUUID(hoglin);
-
-                        if (controllerUUID != null && target.getUUID().equals(controllerUUID)) {
-                            brain.eraseMemory(MemoryModuleType.ATTACK_TARGET);
-                        }
-                    }
+            if (target instanceof Player) {
+                UUID controllerUUID = MobControlledData.getControllerUUID(hoglin);
+                if (controllerUUID != null && target.getUUID().equals(controllerUUID)) {
+                    cir.cancel();
                 }
             }
         }
